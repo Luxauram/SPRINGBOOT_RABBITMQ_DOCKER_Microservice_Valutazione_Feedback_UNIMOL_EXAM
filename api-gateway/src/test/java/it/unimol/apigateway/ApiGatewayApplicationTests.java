7ff1dc0cf12783785195d5663cd79e4212c62310
Luca D'Aurizio <daurizio.lc@gmail.com>
2026-01-18 12:23:58 +0100

    possible third fix to github actions

--- api-gateway/src/test/java/it/unimol/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/it/unimol/apigateway/ApiGatewayApplicationTests.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {
+        "eureka.client.enabled=false",
+        "spring.cloud.discovery.enabled=false",
+        "spring.rabbitmq.host=localhost"
+    }
+)
 @ActiveProfiles("test")
 class ApiGatewayApplicationTests {
 
